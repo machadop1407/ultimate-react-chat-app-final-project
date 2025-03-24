23b2a3eb0cf398ea6c236dd00878c6ea3d75be34
Pedro Henrique Ferraz Machado <pedrotech@Mac.lan>
2025-03-23 17:43:56 -0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { BrowserRouter as Router } from "react-router";
+
+const client = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
